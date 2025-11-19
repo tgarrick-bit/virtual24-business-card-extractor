1cec54b79b550457e7cd9f2292027537beecf1da
TGarrick <tracy@virtual24.ca>
2025-11-19 15:21:43 -0700

    Add West End favicons

---
+++ app/head.tsx
@@ -0,0 +1,20 @@
+export default function Head() {
+    return (
+      <>
+        <title>BuzCard Extractor – W|E</title>
+        <meta
+          name="description"
+          content="West End Workforce business card contact extractor."
+        />
+        {/* Favicons */}
+      <link rel="icon" type="image/png" sizes="32x32" href="/we-icon-32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/we-icon-16.png" />
+
+      {/* iOS home screen icon */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/we-icon-180.png" />
+
+      <meta name="theme-color" content="#e31c79" />
+    </>
+  );
+}
+  
