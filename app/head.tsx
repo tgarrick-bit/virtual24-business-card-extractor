af32546445592352681322dc61748458a6d317d7
TGarrick <tracy@virtual24.ca>
2025-11-19 16:44:20 -0700

    Fix favicon paths; remove legacy favicon.ico; add apple-touch icons

--- app/head.tsx
+++ app/head.tsx
@@ -1,20 +1,23 @@
 export default function Head() {
-    return (
-      <>
-        <title>BuzCard Extractor – W|E</title>
-        <meta
-          name="description"
-          content="West End Workforce business card contact extractor."
-        />
-        {/* Favicons */}
-      <link rel="icon" type="image/png" sizes="32x32" href="/we-icon-32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/we-icon-16.png" />
+  return (
+    <>
+      <title>W|E - Buz Card Extractor</title>
+      <meta
+        name="description"
+        content="West End Workforce business card contact extractor."
+      />
 
-      {/* iOS home screen icon */}
-      <link rel="apple-touch-icon" sizes="180x180" href="/we-icon-180.png" />
+      {/* Favicons from /public */}
+      <link rel="icon" type="image/png" sizes="32x32" href="/we-favicon-32.png?v=3" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/we-favicon-16.png?v=3" />
+
+      {/* iOS Home Screen icon */}
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=3" />
+
+      {/* Optional PWA manifest */}
+      <link rel="manifest" href="/site.webmanifest" />
 
       <meta name="theme-color" content="#e31c79" />
     </>
   );
 }
-  
